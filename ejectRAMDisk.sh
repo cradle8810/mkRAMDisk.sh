7d586af05ccea388a744513837a17f27c32f987b
cradle8810 <falcon.int50m@gmail.com>
2022-02-10 23:22:39 +0900

    Eject時にAPFS物理ストアまでEjectされるので、当該処理は削除

--- ejectRAMDisk.sh
+++ ejectRAMDisk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 MKRAMDISK="/Users/hayato/Scripts/mkRAMDisk.sh"
-RDIR="/Volumes/RAMDisk_1"
+RDIR="/Volumes/RAMDisk"
 
 if [ ! -e "${MKRAMDISK}" ]; then
    echo "No mkramdisk found." >&2
@@ -19,19 +19,8 @@ DEVID="$( ${DISKUTIL} info ${RDIR} | grep "Device Identifier" | awk '{print $3}'
 #RAMDISKデバイス名 (ex)disk2
 DEVNAME="$( ${DISKUTIL} info ${RDIR} | grep "Part of Whole" | awk '{print $4}' )"
 
-# RAMDISKパーティションを含んでいたAPFSコンテナ
-CONTAINERID=$("${DISKUTIL}" info "${DEVNAME}" | grep "APFS Physical Store" | awk '{print $4}')
-# APFSコンテナを含む物理デバイス名
-CONTAINERDEVNAME="$( ${DISKUTIL} info ${CONTAINERID} | grep "Part of Whole" | awk '{print $4}' )"
-
 echo "RAMDisk Partition (eject 1st): ${DEVNAME}"
 echo "RAMDisk Device Name: ${DEVID}"
-echo "Container Partition Name: ${CONTAINERID}"
-echo "Container Device Name (eject 2nd): ${CONTAINERDEVNAME}"
 
-
-echo "Ejecting ${DEVNAME}..."
+echo "Ejecting ..."
 hdiutil detach -force ${DEVNAME}
-
-echo "Ejecting ${CONTAINERDEVNAME}..."
-hdiutil detach -force ${CONTAINERDEVNAME}
